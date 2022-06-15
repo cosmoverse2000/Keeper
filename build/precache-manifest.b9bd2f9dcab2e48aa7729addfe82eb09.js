21a2f474eb033b69322464c161ab3283e8bc62c0
Anshul Patel <anshulpatel5757@gmail.com>
2022-06-16 03:46:44 +0530

    keeper App Completed First Commit

---
+++ build/precache-manifest.b9bd2f9dcab2e48aa7729addfe82eb09.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c83ea285524a7b164080f9d8972cc17",
+    "url": "/Keeper/index.html"
+  },
+  {
+    "revision": "4633edc0369c610a772e",
+    "url": "/Keeper/static/js/2.68c21ef6.chunk.js"
+  },
+  {
+    "revision": "1150a4f96d504150cfd6",
+    "url": "/Keeper/static/js/main.9d57446a.chunk.js"
+  },
+  {
+    "revision": "1abde2512e8fe1574e7d",
+    "url": "/Keeper/static/js/runtime-main.79b73ca4.js"
+  }
+]);
